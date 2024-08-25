754724fb0aa00ea89342a5cc0f83e1ea36003096
Abdul karim Osman <a.osman@bab.com.sa>
2024-08-25 15:25:48 +0300

    8/25PP

--- models/sales_kpi.py
+++ models/sales_kpi.py
@@ -1,31 +1,20 @@
-from odoo import models, fields, api # type: ignore
+from odoo import models, fields
 
 class SalesKPI(models.Model):
     _name = 'sales.kpi'
     _description = 'Sales KPI'
-    _inherit = 'crm.lead'
 
-    # Remove the inherited 'tag_ids' field
-    tag_ids = None
+    name = fields.Char('KPI Name')
+    tag_ids = fields.Many2many(
+        'sales.tag',                # Related model
+        'sales_tag_relation_table', # Unique table name to avoid conflict
+        'kpi_id',                   # Column in the relation table that refers to this model
+        'tag_id',                   # Column in the relation table that refers to the related model
+        string='Tags'
+    )
 
-    # Fields for tracking sales KPIs
-    target_revenue = fields.Float(string='Target Revenue')
-    actual_revenue = fields.Float(string='Actual Revenue')
-    conversion_rate = fields.Float(string='Conversion Rate', compute='_compute_conversion_rate', store=True)
-    kpi_date = fields.Date(string='KPI Date', default=fields.Date.context_today)
+class SalesTag(models.Model):
+    _name = 'sales.tag'
+    _description = 'Sales Tag'
 
-    @api.depends('target_revenue', 'actual_revenue')
-    def _compute_conversion_rate(self):
-        for record in self:
-            if record.target_revenue:
-                record.conversion_rate = (record.actual_revenue / record.target_revenue) * 100
-            else:
-                record.conversion_rate = 0
-
-    def create(self, vals):
-        res = super(SalesKPI, self).create(vals)
-        return res
-
-    def write(self, vals):
-        res = super(SalesKPI, self).write(vals)
-        return res
+    name = fields.Char('Tag Name')
